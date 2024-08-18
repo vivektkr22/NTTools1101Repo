4297f1a681b1cd9f6fa5488dcaaaf2c556f74f02
vivek lodhi <viveklodhi108@gmail.com>
2024-08-19 00:41:43 +0530

    docker web application first commit

---
+++ Docker-TestWebApp/src/main/java/com/nt/DockerTestWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.nt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerTestWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerTestWebAppApplication.class, args);
+	}
+
+}
